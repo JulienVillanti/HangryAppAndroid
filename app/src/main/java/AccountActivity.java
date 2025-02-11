f1dac74cdbee0e6ca423867ef52860e0877696ba
Jéssica Máximo <122978824+jessicamaximo23@users.noreply.github.com>
2025-02-11 11:52:48 -0500

    After choose account open differents intents

--- app/src/main/java/AccountActivity.java
+++ app/src/main/java/AccountActivity.java
@@ -1,2 +1,59 @@
-public class AccountActivity {
+package com.example.hangryappandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_screen);
+
+
+        Button driverButton = findViewById(R.id.driverButton);
+        Button userButton = findViewById(R.id.userButton);
+        Button restaurantButton = findViewById(R.id.restaurantButton);
+        Button adminButton = findViewById(R.id.adminButton);
+
+
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AccountActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir a tela de login do usuário
+                Intent intent = new Intent(AccountActivity.this, UserLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        restaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir a tela de login do restaurante
+                Intent intent = new Intent(AccountActivity.this, RestaurantLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              
+                Intent intent = new Intent(AccountActivity.this, AdminLoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/RestaurantLoginActivity.java
@@ -0,0 +1,2 @@
+public class RestaurantLoginActivity {
+}

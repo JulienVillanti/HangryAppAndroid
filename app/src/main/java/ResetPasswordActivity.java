552554e8f1cdd76ab615b1f943aa72c41d4e3dbb
Jéssica Máximo <122978824+jessicamaximo23@users.noreply.github.com>
2025-02-11 12:57:32 -0500

    just for save, but has many erros for fix and continue later

--- app/src/main/java/ResetPasswordActivity.java
+++ app/src/main/java/ResetPasswordActivity.java
@@ -1,20 +1,26 @@
-import androidx.appcompat.app.AppCompatActivity;
 package com.example.hangryappandroid;
 
 import android.os.Bundle;
-
-
-import com.example.hangryappandroid.R;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
-
+    private EditText editTextEmail;
+    private Button resetPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.resetpassword_screen); 
+        setContentView(R.layout.resetpassword_screen);
 
 
+        editTextEmail = findViewById(R.id.editTextResetEmail);
+        resetPasswordButton = findViewById(R.id.resetPasswordButton);
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() );
     }
 }
--- app/src/main/java/SignUpActivity.java
+++ app/src/main/java/SignUpActivity.java
@@ -1,2 +1,20 @@
-public class SignUpActivity {
+import androidx.appcompat.app.AppCompatActivity;
+package com.example.hangryappandroid;
+
+import android.os.Bundle;
+
+
+import com.example.hangryappandroid.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_screen);
+
+
+    }
 }

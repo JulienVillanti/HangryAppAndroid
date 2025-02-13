128780791222a2bbed7ce490571a279ec42d8eba
Jéssica Máximo <122978824+jessicamaximo23@users.noreply.github.com>
2025-02-13 13:33:27 -0500

    all 3 pages are connect

--- app/src/main/java/com/example/hangryappandroid/SignUpActivity.java
+++ app/src/main/java/com/example/hangryappandroid/SignUpActivity.java
@@ -1,17 +1,62 @@
 package com.example.hangryappandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
-
+    private EditText editTextEmail, editTextPassword;
+    private Button signUpButton;
+    private TextView signInTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_screen);
 
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        signUpButton = findViewById(R.id.singUpBtn);
+        signInTextView = findViewById(R.id.signIn);
 
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(SignUpActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
+                } else if (password.length() < 6) {
+                    Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    Toast.makeText(SignUpActivity.this, "Sign up successful!", Toast.LENGTH_SHORT).show();
+
+                    // Redirecionar para a MainActivity após o cadastro
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+
+        signInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirecionar para a SigninActivity
+                Intent intent = new Intent(SignUpActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

954269ea4a27742b2efdc1400be11281a984631f
Jéssica Máximo <122978824+jessicamaximo23@users.noreply.github.com>
2025-02-14 14:35:20 -0500

    Reset password Done

--- app/src/main/java/com/example/hangryappandroid/ResetPasswordActivity.java
+++ app/src/main/java/com/example/hangryappandroid/ResetPasswordActivity.java
@@ -4,19 +4,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
     private EditText editTextEmail;
     private Button resetPasswordButton;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resetpassword_screen);
 
+        auth = FirebaseAuth.getInstance();
 
         editTextEmail = findViewById(R.id.editTextResetEmail);
         resetPasswordButton = findViewById(R.id.resetPasswordButton);
@@ -24,7 +29,27 @@ public class ResetPasswordActivity extends AppCompatActivity {
         resetPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
 
+                if (email.isEmpty()) {
+                    Toast.makeText(ResetPasswordActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ResetPasswordActivity.this, "Password reset email sent.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Exception exception = task.getException();
+                                if (exception instanceof FirebaseAuthInvalidUserException) {
+
+                                    Toast.makeText(ResetPasswordActivity.this, "Email is not registered.", Toast.LENGTH_SHORT).show();
+                                } else {
+
+                                    Toast.makeText(ResetPasswordActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
